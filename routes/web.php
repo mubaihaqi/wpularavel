8e717c6ec44de9b03f2bb665edea6c9bb0ae6c52
mubaihaqi <muhammadumarbaihaqi93@student.cs.unida.gontor.ac.id>
2025-04-01 18:50:35 +0700

    Single Post

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Arr;
 use Illuminate\View\ViewName;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
@@ -11,8 +12,49 @@ Route::get('/about', function () {
     return view('about', ['nama' => 'Muhammad Umar', 'title' => 'About Page']);
 });
 
-Route::get('/blog', function () {
-    return View('blog', ['title' => 'Blog Page']);
+Route::get('/posts', function () {
+    return View('posts', ['title' => 'Blog Page', 'posts' => [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Muhammad Umar',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis fugit doloremque
+            sapiente voluptatem itaque cumque commodi eligendi neque delectus illum possimus iure aspernatur laborum expedita aliquam earum, repudiandae voluptatum alias.'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Kezia Amara',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem excepturi culpa esse nemo aperiam quidem possimus libero asperiores maiores temporibus? Delectus voluptatum mollitia iusto rerum eaque reprehenderit harum laborum officia?'
+        ]
+    ]]);
+});
+
+Route::get('/posts/{slug}', function ($slug) {
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Muhammad Umar',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis fugit doloremque sapiente voluptatem itaque cumque commodi eligendi neque delectus illum possimus iure aspernatur laborum expedita aliquam earum, repudiandae voluptatum alias.'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Kezia Amara',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem excepturi culpa esse nemo aperiam quidem possimus libero asperiores maiores temporibus? Delectus voluptatum mollitia iusto rerum eaque reprehenderit harum laborum officia?'
+        ]
+    ];
+
+    $post = Arr::first($posts, function ($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
