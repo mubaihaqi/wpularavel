eac1063337d008a21024031786701583628f7082
mubaihaqi <muhammadumarbaihaqi93@student.cs.unida.gontor.ac.id>
2025-04-05 06:48:57 +0700

    Eloquent Relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,9 +3,10 @@
 
     <article class="py-8 max-w-screen-md border-b border-gray-300">
         <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900 hover:text-blue-600">
-            {{ $post['title'] }}</h2>
+            {{ $post->title }}</h2>
         <div class="text-base text-gray-500">
-            <a href="#">{{ $post['author'] }} | {{ $post['created_at']->format('d F Y') }}
+            <a href="/authors/{{ $post->author->id }}">{{ $post->author->name }} |
+                {{ $post->created_at->format('d F Y') }}
         </div>
         <p class="my-4 font-light">{{ $post['body'] }}</p>
         <a href="/posts" class="font-medium text-blue-500 hover:underline">&laquo; Back to posts...</a>
