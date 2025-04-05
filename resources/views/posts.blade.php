f01f0fcf5e5beae46d3dd98dfbbb5c5cef9fbeec
mubaihaqi <muhammadumarbaihaqi93@student.cs.unida.gontor.ac.id>
2025-04-05 12:09:32 +0700

    Second Optimation

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,13 +7,15 @@
                 <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900 hover:text-blue-600">
                     {{ $post['title'] }}</h2>
             </a>
-            <div class="flex items-center text-base text-gray-500 space-x-1">
+            <div class="flex items-center text-base text-slate-600 space-x-1">
                 <p>By</p>
-                <a href="/authors/{{ $post->author->username }}" class="hover:underline">{{ $post->author->name }}
+                <a href="/authors/{{ $post->author->username }}"
+                    class="hover:underline  text-gray-500">{{ $post->author->name }}
                 </a>
                 <p>in </p>
-                <a href="/categories/{{ $post->category->slug }}" class="hover:underline">{{ $post->category->title }}</a>
-                | {{ $post['created_at']->diffForHumans() }}
+                <a href="/categories/{{ $post->category->slug }}"
+                    class="hover:underline  text-gray-500">{{ $post->category->title }}</a>
+                | {{ $post['created_at']->format('d F Y') }}
             </div>
             <p class="my-4 font-light">{{ Str::limit($post['body'], 100) }}</p>
             <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read more...
--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::get('/posts/{post:slug}', function (Post $post) {
 
 Route::get('/authors/{user:username}', function (User $user) {
 
-    return view('posts', ['title' => 'Article by ' . $user->name, 'posts' => $user->posts]);
+    return view('posts', ['title' => count($user->posts) . ' Article by ' . $user->name, 'posts' => $user->posts]);
 });
 
 Route::get('/categories/{category:slug}', function (Category $category) {
